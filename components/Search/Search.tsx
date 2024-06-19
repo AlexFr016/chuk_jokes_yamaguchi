b68f158ea10da85f09ba2c028e769819d9d7b4a8
AleksandrFrolov <SavanationII@yandex.ru>
2024-06-19 22:28:56 +0300

    added dynamic qury params

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,6 +1,7 @@
 import useDebounce from '@/hooks/useDebounce'
 import cn from 'classnames'
-import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useCallback, useEffect, useState } from 'react'
 import styles from './Search.module.css'
 import { ISearchProps } from './Search.props'
 
@@ -12,9 +13,39 @@ export const Search = ({
 	const [inputValue, setInputValue] = useState('')
 
 	const debouncedQuery = useDebounce<string>(inputValue, 400)
+	const router = useRouter()
+
+	const updateQueryParam = useCallback(
+		(query: string) => {
+			if (query.length >= 4) {
+				router.push(
+					{
+						pathname: '/search',
+						query: { query },
+					},
+					undefined,
+					{ shallow: true }
+				)
+			} else {
+				const { query, ...rest } = router.query
+				router.push(
+					{
+						pathname: '/search',
+						query: rest,
+					},
+					undefined,
+					{ shallow: true }
+				)
+			}
+		},
+		[router]
+	)
 
 	useEffect(() => {
 		setSearchQuery(debouncedQuery)
+		console.log('use effect')
+		updateQueryParam(debouncedQuery)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [debouncedQuery, setSearchQuery])
 
 	return (
